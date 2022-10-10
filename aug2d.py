ed2106a7f730ceaf4bc51245d7ffb09b67372064
Mrinal Kanti Dhar <mrinal054@gmail.com>
2022-10-10 00:20:04 -0600

    Update aug2d.py

--- aug2d.py
+++ aug2d.py
@@ -4,3 +4,8 @@ Created on Fri Oct  7 12:42:56 2022
 
 @author: mrinal
 """
+import numpy as np
+from scipy.ndimage.interpolation import shift, zoom
+from scipy.ndimage import rotate
+from skimage import transform
+
