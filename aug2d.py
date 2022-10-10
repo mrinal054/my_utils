6bb1b8169413d55495fb55e7dc21df054d96c14d
Mrinal Kanti Dhar <mrinal054@gmail.com>
2022-10-10 00:20:40 -0600

    Update aug2d.py

--- aug2d.py
+++ aug2d.py
@@ -9,3 +9,11 @@ from scipy.ndimage.interpolation import shift, zoom
 from scipy.ndimage import rotate
 from skimage import transform
 
+class Augmentor2d():
+    """ It performs augmentation on 2D images. It is basically implemented to 
+    perform on-the-fly augmentation. So, to implement a custom ImageDataGenerator 
+    or any data loader, it will come in handy. It performs following augmentation:
+        * Rotation * Flip * Zoom (original image size remains same) 
+        * Shift * Rescale * Shear
+    """
+    
