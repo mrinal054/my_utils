d73977331bd829446b0303f6b77a6b6870e366e8
Mrinal Kanti Dhar <mrinal054@gmail.com>
2022-11-18 18:02:11 -0700

    Update runtime_patch.py

--- runtime_patch.py
+++ runtime_patch.py
@@ -18,3 +18,24 @@ def choose_fg_idx(
     --------
     It returns a foreground index. 
     """
+    if MAX_ROI: # pick the index of the foreground patch that has maximum roi     
+    
+        max_nonzeros = 0 # Maximum no. of nonzeros. Initially set it to 0.
+        final_fg_idx: int # index of the patch that has maximum roi
+        
+        for idx in fg_idx:
+            
+            x = patch_mask[idx] # get the foreground patch mask
+            
+            n_nonzero = np.count_nonzero(x) # no. of nonzeros in the patch mask
+            
+            # Compare with current max no. of nonzeros
+            if n_nonzero > max_nonzeros: 
+                final_fg_idx = idx # update index if new count is higher than the previous count
+                max_nonzeros = n_nonzero # update max no. of nonzeros
+                
+        return final_fg_idx 
+        
+    else: # randomly pick a foreground index
+        return random.choice(fg_idx)
+        
