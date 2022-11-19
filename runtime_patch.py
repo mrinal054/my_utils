36d5cd2b3cde6d550392eccd5d26127fe602d76c
Mrinal Kanti Dhar <mrinal054@gmail.com>
2022-11-18 18:01:33 -0700

    Update runtime_patch.py

--- runtime_patch.py
+++ runtime_patch.py
@@ -9,3 +9,12 @@ def choose_fg_idx(
                            # returns the patch that has maximum info or region of interest (roi) 
         ):
   
+    """ 
+    It is a helper function that picks a foreground index. If MAX_ROI is True,
+    then it returns the index of that patch that has max info or roi in it. Otherwise,
+    it returns a randomly chosen foreground index.
+    
+    Return
+    --------
+    It returns a foreground index. 
+    """
